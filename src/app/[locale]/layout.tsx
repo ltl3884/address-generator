b188a7bee08705689a6c797d387083d9e9b42fc6
ltl3884 <ltl3884@gmail.com>
2025-11-12 16:34:15 +0800

    fix: 修正广告脚本中的crossOrigin属性大小写问题

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -122,7 +122,7 @@ export default async function LocaleLayout({
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet"
         />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6282640794621676" crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6282640794621676" crossOrigin="anonymous"></script>
       </head>
       <body className="antialiased">
         <StructuredData 
